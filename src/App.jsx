0fe82961a270fd9791287aff68ed4d4a6869f744
tolgattli <tolgatatli44@gmail.com>
2023-11-09 17:46:07 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Basket from "./pages/Basket";
+import MyWallet from "./pages/MyWallet";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Fragment>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+      </Routes>
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
